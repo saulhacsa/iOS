f1842007655ed403264a3d4552a016c17931df10
Saúl Godoy <yonoze@gmail.com>
2017-08-14 13:36:34 -0700

    Temporal - sigue sin reconocer el proyecto de la calculadora

---
+++ Ejemplo 1.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground" // implicito
+var str2:String = "hola mundo" // explicito
+
+//tipo de datos
+var Opcional: String? = nil
+var Constante = 20
+
+// ejemplo calculdora
+var n1:Float32 = 5.1
+var n2:Float32 = 5.5
+
+var suma: Float32 = n1 + n2
+var resta: Float32 = n2 - n1
+var multiplicacion: Float32 = n1 * n2
+var division: Float32 = n1 / n2
+
+

ad74c5433818a8bbc197d63ad173c42de29b95e6
Saúl Godoy <yonoze@gmail.com>
2017-08-14 17:05:43 -0700

    Mercedes … navigation controller

--- clase1/digamosunmercedez/digamosunmercedez/ViewController.swift
+++ clase1/digamosunmercedez/digamosunmercedez/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagen: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func imgBtn(_ sender: Any) {
+    //    imagen.image =
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

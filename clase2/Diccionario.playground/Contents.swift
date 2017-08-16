51809a895b8e461aeb52f45b47a7ce7bb6e5195f
Saúl Godoy <yonoze@gmail.com>
2017-08-15 17:44:08 -0700

    update …

---
+++ clase2/Diccionario.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numeroPerros = 5
+
+var cadenaCaracteres:String = "Mi nombre es Juan y tengo \(numeroPerros)"
+var cadenaCaracteres2:String = " perros"
+
+// concatenacion la union de ldos cadenas
+var cadenaCaracteres3:String = cadenaCaracteres + cadenaCaracteres2
+
+// Arreglos &  arrays contenedores especiales,
+var ejemploArray:[String] = ["Arroz", "Pescado", "Cebolla", "Aceite"]
+
+var nombres = ["jose", "pedro", "pablo"]
+print(nombres[0])
+print(nombres[1])
+print(nombres[2])
+
+var num1 : Int = 5
+var num2 : String = "6"
+//var suma : Int = num1 + Int(num2)!
+
+/*
+var dicCadenas:[Int:String] = [1:"uno", 2:"dos", 3:"tres", 4:"cuatro"]
+var dicNumeros:[Int:Int] = [5:15, 6:16, 7:17, 8:18]
+var dicTipificado:[String:String] = ["veinte":"20", "treinta":"30", "cuarenta":"40", "cincuenta":"50"]
+
+let  Nombres:[String:String] = ["saúl":"godoy", "paúl":"lopez", "sergio":"huerta", "karlo":"beaz"]
+*/
+
+
+/*
+let movie:(movie:String, year:Double) = ("Tomorrowland", 2015)
+
+let movieInf = ("Tomorrowland", 2015, 9)
+
+let movie2:(movie:String, año:Double, rank:Int) = ("Tomorrowland", 2015, 9)
+*/
